926295518dab1b8d8e129e715e9b9bf8487a8596
Frans Johansson <fransjohansson98@gmail.com>
2022-10-16 12:21:39 +0200

    Initial setup.py script

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup(name='pylettize',
+      version='pre-0.1',
+      py_modules=['pylettize'],
+      data_files=[('palettes', ['palettes/obama', 'palettes/primaries', 'palettes/gruvbox'])]
+      )

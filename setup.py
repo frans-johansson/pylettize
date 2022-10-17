cef4a152283887d8b265654f1045688571f00f82
Frans Johansson <fransjohansson98@gmail.com>
2022-10-17 20:00:20 +0200

    Format code with black and isort

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup
-setup(name='pylettize',
-      version='pre-0.1',
-      py_modules=['pylettize'],
-      data_files=[('palettes', ['palettes/obama', 'palettes/primaries', 'palettes/gruvbox'])]
-      )
+
+setup(
+    name="pylettize",
+    version="pre-0.1",
+    py_modules=["pylettize"],
+    data_files=[
+        ("palettes", ["palettes/obama", "palettes/primaries", "palettes/gruvbox"])
+    ],
+)

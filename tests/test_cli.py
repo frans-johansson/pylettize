b78d604e8564ed5b8a41fbb081ce00e578b35f53
Frans Johansson <fransjohansson98@gmail.com>
2023-04-07 14:49:58 +0200

    Start adding tests for the CLI module

---
+++ tests/test_cli.py
@@ -0,0 +1,16 @@
+"""Pylettize cli test module."""
+
+import pytest
+from pylettize import cli
+
+
+def test_help_flag_works(monkeypatch, capfd):
+    monkeypatch.setattr("sys.argv", ["pylettize", "-h"])
+
+    with pytest.raises(SystemExit) as e:
+        cli.run()
+
+    assert e.type == SystemExit
+
+    out, _ = capfd.readouterr()
+    assert "Usage:" in out  # Just to make sure the help screen was printed
